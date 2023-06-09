b372b75fe159b70e4e88aa635b7f9c915247619b
Tobias Persson <tobiaspn@axis.com>
2023-06-09 11:58:25 +0200

    Make pylint happy

--- projects/log_listener/src/create_queue/__main__.py
+++ projects/log_listener/src/create_queue/__main__.py
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Create a queue for log listener."""
-import os
-import json
 import time
 from threading import Lock
 from pathlib import Path
