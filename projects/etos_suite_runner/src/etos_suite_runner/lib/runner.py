d42324243cd265807f0626e024de52f9fbfaf30c
Tobias Persson <tobiaspn@axis.com>
2024-12-13 09:00:13 +0100

    Unused import

--- projects/etos_suite_runner/src/etos_suite_runner/lib/runner.py
+++ projects/etos_suite_runner/src/etos_suite_runner/lib/runner.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """ETOS suite runner executor."""
-import os
 import logging
 from multiprocessing.pool import ThreadPool
 
