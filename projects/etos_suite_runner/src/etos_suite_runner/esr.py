c1d1da7f614e1d358e3891cc35fbef5c65131534
Andrei Matveyeu <andreimu@axis.com>
2025-02-07 12:42:41 +0100

    More informative logging when calling release_full_environment()

--- projects/etos_suite_runner/src/etos_suite_runner/esr.py
+++ projects/etos_suite_runner/src/etos_suite_runner/esr.py
@@ -196,12 +196,13 @@ class ESR(OpenTelemetryBase):  # pylint:disable=too-many-instance-attributes
         # Passing variables as keyword argument to make it easier to transition to a function where
         # jsontas is not required.
         jsontas = JsonTas()
-        span_name = "release_full_environment"
+        span_name = "release_full_environment (ESR)"
         with self.otel_tracer.start_as_current_span(
             span_name,
             context=self.otel_context,
             kind=opentelemetry.trace.SpanKind.CLIENT,
         ):
+            self.logger.info("ESR._release_environment(): calling release_full_environment(), suite id: %s", self.params.testrun_id)
             status, message = release_full_environment(
                 etos=self.etos,
                 jsontas=jsontas,
